e36629baecd4ee6c50f1c78a143db51f257f1c56
Rachel Ng <racehl@rachel-ng.com>
2014-01-22 21:36:48 +0000

    Added database and got implemented AR methods

--- application.rb
+++ application.rb
@@ -3,7 +3,6 @@ class Application
   def initialize
     # Start with an empty array of contacts.
     # TODO: Perhaps stub (seed) some contacts so we don't have to create some every time we restart the app
-    @contacts = []
   end
  
   def run
@@ -21,9 +20,6 @@ class Application
       when "show"
         show_contact
 
-      # when "find"
-      #   find_contact
-
       when "quit"
         puts "Goodbye!"
         break
@@ -37,60 +33,47 @@ class Application
     puts " new    - Create a new contact"
     puts " list   - List all contacts"
     puts " show   - Display contact details"
-    puts " find   - Find a contact"
+    # puts " find   - Find a contact"
     print "> "
   end
 
   def new_contact
     puts "What is your full name?"
-    name = gets.chomp
+    name = gets.chomp.split(" ")
     puts "What is your email?"
     email = gets.chomp
     check_email(name, email)
   end
 
   def check_email(name, email)
-    if @contacts.length > 0
-      @contacts.each do |contact|
-      if email == contact.email
-        puts "The email you've entered is already in use"
-      else
-        add_contact(name, email)
-      end
-      end
+    if Contact.find_by email: email
+      puts "The email you've entered is already in use"
     else
       add_contact(name, email)
     end
   end
 
   def add_contact(name, email)
-    contact = Contact.new(name, email)
-    @contacts.push(contact)
+    Contact.create(first_name: name[0], last_name: name[1], email: email)
     puts "Your contact has been added!"
   end
 
   def list_contact
-    @contacts.each do |contact|
-      puts "#{@contacts.index(contact)}: #{contact.first_name} #{contact.last_name[0]} #{contact.email}"
+    Contact.all.each do |contact|
+      puts "#{contact.id}: #{contact.first_name} #{contact.last_name[0]} #{contact.email}"
     end
   end
-
-  # def find_contact
-  #   puts "Who do you want to find?"
-  #   search = gets.chomp
-  #   if 
-  # end
 
   def show_contact
     puts "Enter the ID number you are looking for"
     id = gets.chomp.to_i
-    if @contacts[id] 
-      contact = @contacts[id]
-      puts "#{contact.first_name} #{contact.last_name[0]}" 
-      puts "#{contact.email}"
+    if Contact.find_by id: id
+      contact = Contact.take!
+        puts "#{contact.first_name} #{contact.last_name[0]}" 
+        puts "#{contact.email}"
 
-      puts "Would you like to edit this contact?"
-      input = gets.chomp.downcase
+        puts "Would you like to edit this contact?"
+        input = gets.chomp.downcase
       if input == "yes"
         edit_contact(id)
       else
@@ -103,7 +86,7 @@ class Application
 
   def edit_contact(id)
     puts "Edit name or email?"
-    input = gets.chomp.downcase
+    input = gets.chomp
     if input == "back"
       #figure out what the command is to quit. 'show_main_menu' prints 2 times due to loop.
     else
@@ -120,22 +103,26 @@ class Application
 
   def edit_first_name(id)
     puts "Enter the new first name"
-    new_first_name = gets.chomp.downcase
+    new_first_name = gets.chomp
     if new_first_name == "back"
       #command to quit
     else
-      @contacts[id].first_name = new_first_name
+      contact = Contact.find(id)
+      contact.first_name = new_first_name
+      contact.save
       puts "Your first name has been updated"
     end
   end
 
   def edit_last_name(id)
     puts "Enter the new last name"
-    new_last_name = gets.chomp.downcase
+    new_last_name = gets.chomp
     if new_last_name == "back"
       #command to quit
     else
-      @contacts[id].last_name = new_last_name
+      contact = Contact.find(id)
+      contact.last_name = new_last_name
+      contact.save
       puts "Your last name has been updated"
     end
   end
@@ -146,8 +133,10 @@ class Application
     if new_email == "back"
       #command to quit
     else
-    @contacts[id].email = new_email
-    puts "Your email has been updated"
+      contact = Contact.find(id)
+      contact.email = new_email
+      contact.save
+      puts "Your email has been updated"
     end
   end
  
--- contact.rb
+++ contact.rb
@@ -1,18 +1,4 @@
-class Contact
-  
-  attr_accessor :first_name
-  attr_accessor :last_name
-  attr_accessor :email
-  
-  def initialize(name, email)
-    # TODO: assign local variables to instance variables
-
-    name = name.split(" ")
-    @first_name = name[0]
-    @last_name = name[1]
-    @email = email
-
-  end
+class Contact < ActiveRecord::Base
   
   def to_s
     # TODO: return string representation of Contact
